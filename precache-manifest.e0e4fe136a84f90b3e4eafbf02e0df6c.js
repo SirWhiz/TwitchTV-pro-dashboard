fa023636eecac736031f39e375cb1226a373404a
Alvaro <applewhiz@outlook.es>
2019-08-13 22:19:12 +0200

    Updates

---
+++ precache-manifest.e0e4fe136a84f90b3e4eafbf02e0df6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6917c527800c726f541e2534361806ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f941e8ef59658f3bafe",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "74ff8fefaff9fb611ee7",
+    "url": "/static/css/main.e5aa0c1e.chunk.css"
+  },
+  {
+    "revision": "9f941e8ef59658f3bafe",
+    "url": "/static/js/2.44362449.chunk.js"
+  },
+  {
+    "revision": "74ff8fefaff9fb611ee7",
+    "url": "/static/js/main.07c8f5d4.chunk.js"
+  },
+  {
+    "revision": "2abddc1b60192b73f637",
+    "url": "/static/js/runtime~main.e14b2a2a.js"
+  }
+]);
